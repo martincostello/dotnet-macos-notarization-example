1f010a8ecb0f3866f6696fa1ff48649575a46c08
martincostello <martin@martincostello.com>
2021-05-10 17:40:46 +0100

    Refactor workflow
    Refactor the workflow into build -> notarize -> publish.
    Move some of the steps inline to the workflow.

--- build.ps1
+++ build.ps1
@@ -1,13 +1,2 @@
 #! /usr/bin/pwsh
-
-dotnet publish ./src/HelloWorld/HelloWorld.csproj --output ./artifacts/app --configuration Release --runtime osx-x64
-
-if (Test-Path -Path ./artifacts/HelloWorld.app) {
-    Remove-Item ./artifacts/HelloWorld.app -Recurse -Force | Out-Null
-}
-
-New-Item -Path ./artifacts -Name HelloWorld.app -ItemType "Directory" | Out-Null
-New-Item -Path ./artifacts/HelloWorld.app -Name Contents -ItemType "Directory" | Out-Null
-New-Item -Path ./artifacts/HelloWorld.app/Contents -Name Resources -ItemType "Directory" | Out-Null
-Copy-Item -Path ./artifacts/app -Destination ./artifacts/HelloWorld.app/Contents/MacOS -Recurse
-Copy-Item -Path ./src/HelloWorld/Info.plist -Destination ./artifacts/HelloWorld.app/Contents
+dotnet publish ./src/HelloWorld/HelloWorld.csproj --output ./artifacts/publish --configuration Release --runtime osx-x64

0acb35112a3843c1613e7a257c611c9862c19ceb
martincostello <martin@martincostello.com>
2021-05-10 14:38:39 +0100

    Try a different signing mechanism
    Try a different mechanism for code signing.

---
+++ sign.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+APP_NAME="./HelloWorld.app"
+ENTITLEMENTS="./HelloWorld.entitlements"
+
+find "$APP_NAME/Contents/MacOS/"|while read fname; do
+    if [[ -f $fname ]]; then
+        echo "[INFO] Signing $fname"
+        codesign --force --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" "$fname"
+    fi
+done
+
+echo "[INFO] Signing app file"
+
+codesign --force --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" "$APP_NAME"

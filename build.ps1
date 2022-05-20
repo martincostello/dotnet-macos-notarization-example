40ab9512bcf39515065cea8a089aa5e28fd3cd03
martincostello <martin@martincostello.com>
2022-05-20 10:57:35 +0100

    Fix NETSDK1179 warning
    Fix NETSDK1179 warning by specifying `--no-self-contained`.

--- build.ps1
+++ build.ps1
@@ -1,2 +1,7 @@
 #! /usr/bin/env pwsh
-dotnet publish ./src/HelloWorld/HelloWorld.csproj --output ./artifacts/publish --configuration Release --runtime osx-x64
+dotnet publish `
+       ./src/HelloWorld/HelloWorld.csproj `
+       --output ./artifacts/publish `
+       --configuration Release `
+       --runtime osx-x64 `
+       --no-self-contained
